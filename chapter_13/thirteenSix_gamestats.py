30d0c0eed2775eb24604838837c9273d1fc89805
kelvDp <63354814+kelvDp@users.noreply.github.com>
2020-07-24 16:05:10 +0200

    Update thirteenSix_gamestats.py

--- chapter_13/thirteenSix_gamestats.py
+++ chapter_13/thirteenSix_gamestats.py
@@ -5,11 +5,21 @@ class GameStats:
         """Initialize statistics"""
 
         self.settings = ai_game.settings
+        self.reset_stats()
 
-        # game active status for TIY_13-6
-        self.game_active = True
+        self.game_active = False
 
         # game active status for TIY_14-1
         self.second_game_active = False
 
+        # high scores should never be reset:
+        with open("highscoreThirteen.txt") as f:
+            score = int(f.readline())
+        self.high_score = score
+
+    def reset_stats(self):
+        """Inits stats that can change during game"""
+
         self.ships_left = self.settings.ship_limit
+        self.score = 0
+        self.level = 1
